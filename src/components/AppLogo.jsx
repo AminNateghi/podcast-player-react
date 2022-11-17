c92f40d4b319c5027ddc9ccf88038453db39373a
Amin Nateghi <amin.programmer@gmail.com>
2022-11-17 17:38:06 +0300

    added appLogo component

---
+++ src/components/AppLogo.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import styles from './AppLogo.module.scss';
+
+export default class AppLogo extends Component {
+  render() {
+    return (
+      <div className={styles.logoContainer}>
+        <h5 className={styles.title}>Radio Player</h5>
+      </div>
+    )
+  }
+}
