594ce494a197dd25050281e7f644cf77de18a4f1
Amin Nateghi <amin.programmer@gmail.com>
2022-11-27 20:18:53 +0300

    added props to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,19 @@ import styles from './Header.module.scss';
 import IconMenu from './icons/Menu';
 import { InputSearch } from './InputSearch';
 
-export const Header = ({ handleSearch }) => {
+export const Header = ({ title, showSearch, handleSearch }) => {
   return (
     <div className={ styles.headerContainer }>
       <div className={ styles.content }>
-        <h5 hidden className={ styles.title }>All Countries</h5>
+        <h5 hidden className={ styles.title }>{ title }</h5>
         <button className={ styles.deviceMenu }>
           <IconMenu />
         </button>
         <div className="flex space-x-4">
-          <InputSearch handleSearch={ handleSearch } />
+          { showSearch
+            ? <InputSearch handleSearch={ handleSearch } />
+            : null
+          }
         </div>
       </div>
     </div>
