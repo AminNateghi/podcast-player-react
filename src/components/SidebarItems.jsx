0ede9759d88bea649d6128d7709f85bfd8546a00
Amin Nateghi <amin.programmer@gmail.com>
2022-11-27 23:00:05 +0300

    change sidebar to arrow function

--- src/components/SidebarItems.jsx
+++ src/components/SidebarItems.jsx
@@ -1,45 +1,43 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 import IconCategory from './icons/Category'
 import IconDashboard from './icons/Dashboard'
 import IconSearch from './icons/Search';
 import styles from './SidebarItems.module.scss';
 
-export default class SidebarItems extends Component {
-  render() {
-    return (
-      <ul className={ styles.sidebarContainer }>
-        <li>
-          <NavLink to={ "best-podcast" } className={ ({ isActive }) =>
-            isActive
-              ? styles.active
-              : styles.normal
-          }>
-            <IconDashboard />
-            <span>Best Podcasts</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ "search" } className={ ({ isActive }) =>
-            isActive
-              ? styles.active
-              : styles.normal
-          }>
-            <IconSearch />
-            <span>Search</span>
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ "category" } className={ ({ isActive }) =>
-            isActive
-              ? styles.active
-              : styles.normal
-          }>
-            <IconCategory />
-            <span>Categories</span>
-          </NavLink>
-        </li>
-      </ul>
-    )
-  }
+export const SidebarItems = () => {
+  return (
+    <ul className={ styles.sidebarContainer }>
+      <li>
+        <NavLink to={ "best-podcast" } className={ ({ isActive }) =>
+          isActive
+            ? styles.active
+            : styles.normal
+        }>
+          <IconDashboard />
+          <span>Best Podcasts</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={ "search" } className={ ({ isActive }) =>
+          isActive
+            ? styles.active
+            : styles.normal
+        }>
+          <IconSearch />
+          <span>Search</span>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={ "category" } className={ ({ isActive }) =>
+          isActive
+            ? styles.active
+            : styles.normal
+        }>
+          <IconCategory />
+          <span>Categories</span>
+        </NavLink>
+      </li>
+    </ul>
+  )
 }
