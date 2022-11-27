d1dd5ae47fe0615f0bfd0e0c12a45f10873b6d24
Amin Nateghi <amin.programmer@gmail.com>
2022-11-27 20:21:21 +0300

    added item to constant file

--- src/constant/app.const.jsx
+++ src/constant/app.const.jsx
@@ -3,3 +3,4 @@ import { Client } from 'podcast-api';
 export const PODCAST_API_CLIENT = Client({ apiKey: null }); // free plan of ListenNotes.com api
 export const BEST_PODCAST_ID = 'bestPodcast';
 export const SEARCH_RESULT_ID = 'searchResult';
+export const SEARCH_KEYWORD_ID = 'searchKeyword';
