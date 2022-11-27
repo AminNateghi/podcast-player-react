71f7f8246c4404fbf8f0a3997dbc4c453ddc37fa
Amin Nateghi <amin.programmer@gmail.com>
2022-11-27 20:20:31 +0300

    added method to inputSearch component

--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import styles from './InputSearch.module.scss';
 
-export const InputSearch = ({ handleSearch }) => {
+export const InputSearch = () => {
+  const navigate = useNavigate();
+
+  const handleSearch = (event) => {
+    if (event.key === 'Enter') {
+      const value = event.target?.value;
+      navigate('/search', { state: { query: value } });
+    }
+  }
+
   return (
     <div hidden className="md:block">
       <div className={ styles.inputSearchContainer }>
@@ -10,7 +20,7 @@ export const InputSearch = ({ handleSearch }) => {
             <path id="Icon_awesome-search" data-name="search" d="M35.508,31.127l-7.01-7.01a1.686,1.686,0,0,0-1.2-.492H26.156a14.618,14.618,0,1,0-2.531,2.531V27.3a1.686,1.686,0,0,0,.492,1.2l7.01,7.01a1.681,1.681,0,0,0,2.384,0l1.99-1.99a1.7,1.7,0,0,0,.007-2.391Zm-20.883-7.5a9,9,0,1,1,9-9A8.995,8.995,0,0,1,14.625,23.625Z"></path>
           </svg>
         </span>
-        <input type="search" name="leadingIcon" id="leadingIcon" placeholder="Search here" className="w-full pl-14 pr-4 py-2.5 rounded-xl text-sm text-gray-600 outline-none border border-gray-300 focus:border-cyan-300 transition" onKeyDown={ event => handleSearch(event) } />
+        <input type="search" name="search" placeholder="Search here" className="w-full pl-14 pr-4 py-2.5 rounded-xl text-sm text-gray-600 outline-none border border-gray-300 focus:border-cyan-300 transition" onKeyDown={ event => handleSearch(event) } />
       </div>
     </div>
   )
