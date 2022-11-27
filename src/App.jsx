a2b80535ce665e8713051145f5bbee82a0a709bf
Amin Nateghi <amin.programmer@gmail.com>
2022-11-28 01:56:28 +0300

    added ListenNote image to grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import styles from './App.module.scss';
 import { Sidebar } from "./components/Sidebar";
 import { Outlet } from 'react-router-dom';
+import IconPoweredBy from './assets/images/listennotes.png';
 
 export const App = () => {
   const [currentRadio, setCurrentRadio] = useState({});
@@ -13,6 +14,11 @@ export const App = () => {
         <div className={ styles.appContainer }>
           <div className={ styles.contentGrid }>
             <Outlet />
+          </div>
+          <div className={ styles.poweredBy }>
+            <a target="_blank" href="https://www.listennotes.com/" rel="noreferrer">
+              <img alt="Search powered by ListenNotes" src={ IconPoweredBy } width="110px" />
+            </a>
           </div>
         </div>
       </div>
