26fbb01f205b32d4e8eb1d5e234e5908ea6b51fc
Amin Nateghi <amin.programmer@gmail.com>
2023-01-09 00:13:16 +0300

    cleanups

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styles from './App.module.scss';
-import { Sidebar } from "./components/Sidebar";
+import { Sidebar } from './components/Sidebar';
 import { Outlet } from 'react-router-dom';
-import IconPoweredBy from './assets/images/listennotes.png';
 
 export const App = () => {
-  const [currentRadio, setCurrentRadio] = useState({});
+  const [currentRadio] = useState({});
 
   return (
     <>
-      <Sidebar currentRadio={ currentRadio } />
-      <div className="ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
-        <div className={ styles.appContainer }>
-          <div className={ styles.contentGrid }>
+      <Sidebar currentRadio={currentRadio} />
+      <div className='ml-auto mb-6 lg:w-[75%] xl:w-[80%] 2xl:w-[85%]'>
+        <div className={styles.appContainer}>
+          <div className={styles.contentGrid}>
             <Outlet />
-          </div>
-          <div className={ styles.poweredBy }>
-            <a target="_blank" href="https://www.listennotes.com/" rel="noreferrer">
-              <img alt="Search powered by ListenNotes" src={ IconPoweredBy } width="110px" />
-            </a>
           </div>
         </div>
       </div>
     </>
-  )
-}
-
+  );
+};
